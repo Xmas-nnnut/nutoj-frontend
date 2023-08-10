94eb24cc44f83a6531bbe370f35af83fb7b77ed6
xuqingjian <xuqingjian894@gmail.com>
2023-08-10 15:54:32 +0800

    feat:整合 Markdown 编辑器

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ArcoVue from "@arco-design/web-vue";
+import "bytemd/dist/index.css";
 import "@arco-design/web-vue/dist/arco.css";
 import "@/plugins/axios";
 import "@/access";
